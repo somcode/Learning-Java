80a09bd6cb43e0b20cdd85c3021471239af28bcd
Somayeh <mail.somayeh@gmail.com>
2018-07-23 20:06:23 +0100

    Declaration and Instantiation of Array

---
+++ src/TestArray.java
@@ -0,0 +1,18 @@
+
+public class TestArray {
+	public static void main(String[] args) {
+		
+		int a[] = new int[4]; // DEclaration and Instantiation
+		
+		a[0] = 10; // Initialization
+		a[1] = 20;
+		a[2] = 30;
+		a[3] = 40;
+		
+		for(int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+		
+	}
+
+}
